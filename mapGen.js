e00e43f13b9f3256f1c0afc2a306415df6782150
noppaz <32292420+noppaz@users.noreply.github.com>
2018-01-26 00:25:30 +0100

    Added click and zoom to functionality

--- mapGen.js
+++ mapGen.js
@@ -5,7 +5,7 @@ function initMap() {
 	var map = new mapboxgl.Map({
 		container: 'map',
 		style: 'mapbox://styles/mapbox/light-v9',
-		center: [20, 45],
+		center: [20, 55],
 		zoom: 2
 	});
 
@@ -26,32 +26,62 @@ function initMap() {
 		});
 	});
 
-	buildLocationList(peaks);
+	buildPeakList(peaks, map);
+
+	// Add an event listener for when a user clicks on the map
+	map.on('click', function(e) {
+		// Query all the rendered points in the view
+		var features = map.queryRenderedFeatures(e.point, { layers: ['peaks'] });
+		if (features.length) {
+			var clickedPoint = features[0];
+			// 1. Fly to the point
+			flyToPeak(clickedPoint, map);
+			// 2. Close all other popups and display popup for clicked store
+			createPopUp(clickedPoint, map);
+			// 3. Highlight listing in sidebar (and remove highlight for all other listings)
+			var activeItem = document.getElementsByClassName('active');
+			if (activeItem[0]) {
+			  activeItem[0].classList.remove('active');
+			}
+			// Find the index of the store.features that corresponds to the clickedPoint that fired the event listener
+			var selectedFeature = clickedPoint.properties.name;
+
+			for (var i = 0; i < peaks.features.length; i++) {
+			  if (peaks.features[i].properties.name === selectedFeature) {
+			    var selectedFeatureIndex = i;
+			  }
+			}
+			// Select the correct list item using the found index and add the active class
+			var listing = document.getElementById('listing-' + selectedFeatureIndex);
+			listing.classList.add('active');
+		}
+		});
 }
 
 
-function buildLocationList(data) {
-  // Iterate through the list of stores
+// ##############################################################
+// ------------------- Listings builder -------------------------
+// ##############################################################
+
+function buildPeakList(data, map) {
+  // Iterate through the list of peaks
   for (i = 0; i < data.features.length; i++) {
     var currentFeature = data.features[i];
     var prop = currentFeature.properties;
-    // Select the listing container in the HTML and append a div
-    // with the class 'item' for each store
+
     var listings = document.getElementById('listings');
     var listing = listings.appendChild(document.createElement('div'));
     listing.className = 'item';
     listing.id = 'listing-' + i;
+    console.log(listing.id);
 
-    // Create a new link with the class 'title' for each store
-    // and fill it with the store address
     var link = listing.appendChild(document.createElement('a'));
     link.href = '#';
     link.className = 'title';
     link.dataPosition = i;
     link.innerHTML = prop.name;
 
-    // Create a new div with the class 'details' for each store
-    // and fill it with the city and phone number
+    // Add details from properties
     var details = listing.appendChild(document.createElement('div'));
     details.innerHTML = prop.country;
     var details2 = listing.appendChild(document.createElement('div'));
@@ -61,14 +91,61 @@ function buildLocationList(data) {
     details3.innerHTML = prop.completed;
     var details4 = listing.appendChild(document.createElement('div'));
     details4.innerHTML = prop.link;
-    // if (prop.phone) {
-    //   details.innerHTML += ' &middot; ' + prop.phoneFormatted;
-    // }
+
+    // Event listener
+	link.addEventListener('click', function(e) {
+		console.log("list click");
+		var clickedListing = data.features[this.dataPosition];
+
+		flyToPeak(clickedListing, map);
+
+		createPopUp(clickedListing, map);
+
+		var activeItem = document.getElementsByClassName('active');
+		if (activeItem[0]) {
+			activeItem[0].classList.remove('active');
+		}
+		this.parentNode.classList.add('active');
+	});
   }
 }
 
+// ##############################################################
+// ------------------- Interaction funcs ------------------------
+// ##############################################################
 
-//####################################################################
+
+function flyToPeak(currentFeature, map) {
+	var zoomlvl;
+	if (map.getZoom() > 6) {
+		zoomlvl = map.getZoom();
+	} else {
+		zoomlvl = 6
+	}
+
+	map.flyTo({
+		center: currentFeature.geometry.coordinates,
+		zoom: zoomlvl
+	});
+	console.log(zoomlvl);
+}
+
+function createPopUp(currentFeature, map) {
+	var popUps = document.getElementsByClassName('mapboxgl-popup');
+
+	if (popUps[0]) popUps[0].remove(); //Remove existing popup
+
+	var popup = new mapboxgl.Popup({})
+	.setLngLat(currentFeature.geometry.coordinates)
+	.setHTML('<h3>' + currentFeature.properties.name + '</h3>' +
+	  '<h4>' + currentFeature.properties.country + ' | ' + currentFeature.properties.height + ' m</h4>')
+	.addTo(map);
+}
+
+
+// ##############################################################
+// -------------------------- Data ------------------------------
+// ##############################################################
 
 function dataInit() {
 	var peaks = {
